ee59ef9f16bbec57d69b3c7fc887afecd82e714d
akki2754 <sutarakshay2754@gmail.com>
2021-05-29 18:43:22 +0530

    add multiplication function for two numbers

---
+++ mul1.js
@@ -0,0 +1,8 @@
+const mul = (firstNumber, secondNumber) => {
+    return firstNumber * secondNumber;
+}
+
+console.log(mul(3,2));
+
+9f870585754353dbc7509cb9ef3ee7cf6af2ddaa 
+7e79dbeac7ba6036fb1c75781c31b8d77ab3b56f
